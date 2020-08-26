481c8a4237da15c43b57cc28e0c144bd7b952b5a
avkorchagin <amatjkay@gmail.com>
2020-08-26 13:07:46 +0300

    Добавление параметра addoption --language

--- NLO.py
+++ NLO.py
@@ -5,14 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-@pytest.fixture(scope="session")
-def browser():
-	print("\nstart browser for test..")
-	browser = webdriver.Chrome()
-	yield browser
-	print("\nquit browser..")
-	browser.quit()
-
 @pytest.fixture(scope="function")
 def answer():
 	answer = math.log(int(time.time()))
